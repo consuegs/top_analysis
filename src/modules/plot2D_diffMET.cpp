a8dc1070046da3dbab5c5bebb58332a8a475cf91
Danilo Meuser <danilo.meuser@rwth-aachen.de>
2019-05-20 10:10:36 +0200

    added highMet selection

--- src/modules/plot2D_diffMET.cpp
+++ src/modules/plot2D_diffMET.cpp
@@ -22,98 +22,103 @@ void run()
    io::RootFileReader histReader(TString::Format("histograms_%s.root",cfg.treeVersion.Data()),TString::Format("distributions%.1f",cfg.processFraction*100));
    TCanvas can;
    
-   
-   for (TString sSample :{"TTbar","SingleTop","WJetsToLNu","DrellYan","WW","WZ","ZZ",
-   "T1tttt_1200_800","T2tt_650_250","T2tt_850_100","DM_pseudo_50_50","DM_scalar_10_10","DM_scalar_1_200"}){
-   
-      for (TString sVar :{"2d_nunuVSgenMet","MetVSgenMet","2d_nunuVSMet","2d_nunuVSDMgenMet","MetVSDMgenMet"}) {
-         
-         TH2F hist_add;
-         TH2F *hist;
-         
-         for (TString cat:{"ee","emu","mumu","all"}){
+   for (TString sSelection : {"genParticles","genParticles_Met200"}){
+      for (TString sSample :{"TTbar","SingleTop","WJetsToLNu","DrellYan","WW","WZ","ZZ",
+      "T1tttt_1200_800","T2tt_650_250","T2tt_850_100","DM_pseudo_50_50","DM_scalar_10_10","DM_scalar_1_200"}){
+      
+         for (TString sVar :{"2d_nunuVSgenMet","MetVSgenMet","2d_nunuVSMet","2d_nunuVSDMgenMet","MetVSDMgenMet"}) {
             
-            if (cat!="all"){
-               hist = (TH2F*) histReader.read<TH2F>("genParticles/"+cat+"/"+sVar+"/"+sSample);
-               if (cat=="ee") hist_add=(TH2F) *(histReader.read<TH2F>("genParticles/"+cat+"/"+sVar+"/"+sSample));
-               else hist_add.Add(hist);
-            }
-            else {
-               hist = &hist_add;
-            }
+            TH2F hist_add;
+            TH2F *hist;
+            
+            for (TString cat:{"ee","emu","mumu","all"}){
                
-            
-            hist->RebinX(2);
-            hist->RebinY(2);
-            
-            for (TString norm:{"","LineNorm"}){
-               
-               if (norm==""){
-                  hist->Scale(1.0/(hist->Integral()));
+               //Add histograms for category all, otherwise use only hist (not hist_add)
+               if (cat!="all"){
+                  hist = (TH2F*) histReader.read<TH2F>(sSelection+"/"+cat+"/"+sVar+"/"+sSample);
+                  if (cat=="ee") hist_add=(TH2F) *(histReader.read<TH2F>(sSelection+"/"+cat+"/"+sVar+"/"+sSample));
+                  else hist_add.Add(hist);
                }
-               else{
-                  //Normalize each individual line of diagram
-                  float sum;
-                  for (int y=1; y<=hist->GetNbinsY(); y++){
-                     sum=hist->Integral(1,hist->GetNbinsX(),y,y);
-                     if (sum==0) continue;
-                     for (int x=1; x<=hist->GetNbinsX(); x++){
-                        hist->SetBinContent(x,y,hist->GetBinContent(x,y)/sum);
+               else {
+                  hist = &hist_add;
+               }
+                  
+               
+               hist->RebinX(2);
+               hist->RebinY(2);
+               
+               for (TString norm:{"","LineNorm"}){
+                  
+                  if (norm==""){
+                     hist->Scale(1.0/(hist->Integral()));
+                  }
+                  else{
+                     //Normalize each individual line of diagram
+                     float sum;
+                     for (int y=1; y<=hist->GetNbinsY(); y++){
+                        sum=hist->Integral(1,hist->GetNbinsX(),y,y);
+                        if (sum==0) continue;
+                        for (int x=1; x<=hist->GetNbinsX(); x++){
+                           hist->SetBinContent(x,y,hist->GetBinContent(x,y)/sum);
+                        }
                      }
                   }
-               }
-               
-               TGraph diag;
-               diag.SetPoint(1,0,0);
-               diag.SetPoint(2,600,600);
-               can.cd();
-               can.SetLogz();
-               
-               gPad->SetRightMargin(0.2);
-               gPad->SetLeftMargin(0.13);
-               gPad->SetBottomMargin(0.11);
-               
-               hist->GetYaxis()->SetTitleOffset(1.3);
-               hist->GetXaxis()->SetTitleOffset(0.9);
-               hist->GetZaxis()->SetTitleOffset(1.3);
-               hist->GetYaxis()->SetTitleSize(0.05);
-               hist->GetXaxis()->SetTitleSize(0.05);
-               hist->GetZaxis()->SetTitleSize(0.05);
-               hist->GetYaxis()->SetLabelSize(0.04);
-               hist->GetXaxis()->SetLabelSize(0.04);
-               hist->GetZaxis()->SetLabelSize(0.04);
-               
-               if (norm==""){
-                  hist->GetZaxis()->SetTitle("normalized distribution");
                   
-                  hist->SetMinimum(0.000001);
-                  hist->SetMaximum(0.1);
-               }
-               else{
-                  hist->GetZaxis()->SetTitle("line normalized distribution");
+                  TGraph diag;
+                  diag.SetPoint(1,0,0);
+                  diag.SetPoint(2,600,600);
+                  can.cd();
+                  can.SetLogz();
                   
-                  hist->SetMinimum(0.00001);
-                  hist->SetMaximum(1.);
-               }
+                  gPad->SetRightMargin(0.2);
+                  gPad->SetLeftMargin(0.13);
+                  gPad->SetBottomMargin(0.11);
+                  
+                  hist->GetYaxis()->SetTitleOffset(1.3);
+                  hist->GetXaxis()->SetTitleOffset(0.9);
+                  hist->GetZaxis()->SetTitleOffset(1.3);
+                  hist->GetYaxis()->SetTitleSize(0.05);
+                  hist->GetXaxis()->SetTitleSize(0.05);
+                  hist->GetZaxis()->SetTitleSize(0.05);
+                  hist->GetYaxis()->SetLabelSize(0.04);
+                  hist->GetXaxis()->SetLabelSize(0.04);
+                  hist->GetZaxis()->SetLabelSize(0.04);
+                  
+                  if (norm==""){
+                     hist->GetZaxis()->SetTitle("normalized distribution");
+                     
+                     hist->SetMinimum(0.000001);
+                     hist->SetMaximum(0.1);
+                  }
+                  else{
+                     hist->GetZaxis()->SetTitle("line normalized distribution");
+                     
+                     hist->SetMinimum(0.00001);
+                     hist->SetMaximum(1.);
+                  }
 
-               hist->SetStats(false);
-               hist->Draw("colz");
-               diag.Draw("same");
-               
-               TString cat_label=cat;
-               if (cat=="emu") cat_label="e#mu";
-               else if (cat=="mumu") cat_label="#mu#mu";
-               else if (cat=="all") cat_label="all";
-               TLatex label=gfx::cornerLabel(cat_label+"    "+sSample,1);
-               label.Draw();
-               
-               can.RedrawAxis();
-               TString plotLoc=sSample+"/"+sVar+norm+"/"+cat;
-               saver.save(can,plotLoc,true,true);
-               can.Clear();
+                  hist->SetStats(false);
+                  hist->Draw("colz");
+                  diag.Draw("same");
+                  
+                  TString cat_label=cat;
+                  if (cat=="emu") cat_label="e#mu";
+                  else if (cat=="mumu") cat_label="#mu#mu";
+                  else if (cat=="all") cat_label="all";
+                  TString labelString=cat_label+"    "+sSample;
+                  if (sSelection=="genParticles_Met200") labelString=cat_label+"    "+sSample+"    p_{T}^{miss}>200 GeV";
+                  TLatex label=gfx::cornerLabel(labelString,1);
+                  label.Draw();
+                  
+                  can.RedrawAxis();
+                  TString plotLoc=sSample+"/"+sVar+norm+"/"+cat;
+                  if (sSelection=="genParticles_Met200") plotLoc="Met200/"+sSample+"/"+sVar+norm+"/"+cat;
+                  saver.save(can,plotLoc,true,true);
+                  can.Clear();
+               }
             }
+            
          }
-         
       }
    }
 }
--- src/modules/plot_diffMET.cpp
+++ src/modules/plot_diffMET.cpp
@@ -22,60 +22,64 @@ void run()
    io::RootFileReader histReader(TString::Format("histograms_%s.root",cfg.treeVersion.Data()),TString::Format("distributions%.1f",cfg.processFraction*100));
    TCanvas can;
    
-   
-   for (TString sSample :{"TTbar","SingleTop","WJetsToLNu","DrellYan","WW","WZ","ZZ",
-   "T1tttt_1200_800","T2tt_650_250","T2tt_850_100","DM_pseudo_50_50","DM_scalar_10_10","DM_scalar_1_200"}){
-   
-      for (TString sVar :{"diff_ptNuNu_genMET","diff_Met_genMET","diff_ptNuNu_Met","diff_Met_genMET_norm","diff_Met_DMgenMET_norm","diff_ptNuNu_DMgenMET","diff_Met_DMgenMET"}) {
-         
-         TH1F hist_add;
-         TH1F *hist;
-         
-         for (TString cat:{"ee","emu","mumu","all"}){
+   for (TString sSelection : {"genParticles","genParticles_Met200"}){
+      for (TString sSample :{"TTbar","SingleTop","WJetsToLNu","DrellYan","WW","WZ","ZZ",
+      "T1tttt_1200_800","T2tt_650_250","T2tt_850_100","DM_pseudo_50_50","DM_scalar_10_10","DM_scalar_1_200"}){
+      
+         for (TString sVar :{"diff_ptNuNu_genMET","diff_Met_genMET","diff_ptNuNu_Met","diff_Met_genMET_norm","diff_Met_DMgenMET_norm","diff_Met_genMET_normSUM","diff_Met_DMgenMET_normSUM","diff_ptNuNu_DMgenMET","diff_Met_DMgenMET"}) {
             
-            if (cat!="all"){
-               hist=(TH1F*) histReader.read<TH2F>("genParticles/"+cat+"/"+sVar+"/"+sSample);
-               if (cat=="ee") hist_add=(TH1F) *(histReader.read<TH1F>("genParticles/"+cat+"/"+sVar+"/"+sSample));
-               else hist_add.Add(hist);
-            }
-            else {
-               hist = &hist_add;
-            }
+            TH1F hist_add;
+            TH1F *hist;
             
-            if (sVar!="diff_ptNuNu_genMET" and sVar!="diff_Met_genMET_norm" and sVar!="diff_Met_DMgenMET_norm") hist->RebinX(4);
-            //~ else hist->RebinX(2);
-            
-            hist->Scale(1.0/(hist->Integral()));
-            
-            can.cd();
-            can.SetLogz();
-            
-            hist->GetYaxis()->SetTitle("normalized distribution");
+            for (TString cat:{"ee","emu","mumu","all"}){
+               
+               if (cat!="all"){
+                  hist=(TH1F*) histReader.read<TH2F>(sSelection+"/"+cat+"/"+sVar+"/"+sSample);
+                  if (cat=="ee") hist_add=(TH1F) *(histReader.read<TH1F>(sSelection+"/"+cat+"/"+sVar+"/"+sSample));
+                  else hist_add.Add(hist);
+               }
+               else {
+                  hist = &hist_add;
+               }
+               
+               if (sVar!="diff_ptNuNu_genMET" and sVar!="diff_Met_genMET_norm" and sVar!="diff_Met_DMgenMET_norm" and sVar!="diff_Met_genMET_normSUM" and sVar!="diff_Met_DMgenMET_normSUM") hist->RebinX(4);
+               //~ else hist->RebinX(2);
+               
+               hist->Scale(1.0/(hist->Integral()));
+               
+               can.cd();
+               can.SetLogz();
+               
+               hist->GetYaxis()->SetTitle("normalized distribution");
 
-            hist->Fit("gaus","Q");
-            
-            if (sVar=="diff_Met_genMET_norm"){
-               hist->GetXaxis()->SetTitle("(p_{T}^{miss}-genMET)/genMET");
-               hist->GetXaxis()->SetRangeUser(-5,5);
+               hist->Fit("gaus","Q");
+               
+               if (sVar=="diff_Met_genMET_norm"){
+                  hist->GetXaxis()->SetTitle("(p_{T}^{miss}-genMET)/genMET");
+                  hist->GetXaxis()->SetRangeUser(-5,5);
+               }
+               else if (sVar=="diff_Met_DMgenMET_norm"){
+                  hist->GetXaxis()->SetTitle("(p_{T}^{miss}-DMgenMET)/DMgenMET");
+                  hist->GetXaxis()->SetRangeUser(-5,5);
+               }
+               gStyle->SetStatY(0.9);
+               hist->Draw();
+               
+               TString cat_label=cat;
+               if (cat=="emu") cat_label="e#mu";
+               else if (cat=="mumu") cat_label="#mu#mu";
+               else if (cat=="all") cat_label="all";
+               TString labelString=cat_label+"    "+sSample;
+               if (sSelection=="genParticles_Met200") labelString=cat_label+"    "+sSample+"    p_{T}^{miss}>200 GeV";
+               TLatex label=gfx::cornerLabel(labelString,1);
+               label.Draw();
+               
+               can.RedrawAxis();
+               TString plotLoc=sSample+"/"+sVar+"/"+cat;
+               if (sSelection=="genParticles_Met200") plotLoc="Met200/"+sSample+"/"+sVar+"/"+cat;
+               saver.save(can,plotLoc,true,true);
+               can.Clear();
             }
-            else if (sVar=="diff_Met_DMgenMET_norm"){
-               hist->GetXaxis()->SetTitle("(p_{T}^{miss}-DMgenMET)/DMgenMET");
-               hist->GetXaxis()->SetRangeUser(-5,5);
-            }
-            gStyle->SetStatY(0.9);
-            hist->Draw();
-            
-            TString cat_label=cat;
-            if (cat=="emu") cat_label="e#mu";
-            else if (cat=="mumu") cat_label="#mu#mu";
-            else if (cat=="all") cat_label="all";
-            TLatex label=gfx::cornerLabel(cat_label+"    "+sSample,1);
-            label.Draw();
-            
-            can.RedrawAxis();
-            TString plotLoc=sSample+"/"+sVar+"/"+cat;
-            saver.save(can,plotLoc,true,true);
-            can.Clear();
          }
       }
    }
