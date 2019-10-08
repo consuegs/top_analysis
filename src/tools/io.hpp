3dd638fd2776f84f8bfe809b9a8825c1ecc99079
Danilo Meuser <danilo.meuser@rwth-aachen.de>
2019-10-08 10:53:41 +0200

    Added close file method

--- src/tools/io.hpp
+++ src/tools/io.hpp
@@ -27,6 +27,7 @@ namespace io
       ~RootFileSaver();
       void save(TObject const &obj, TString name, bool decorate=true,bool simulation=true) const;
       void save(gfx::SplitCan &obj, TString name,bool simulation=true) const;
+      void closeFile() const;
    private:
       TFile *file_;
       TString fName_,fPath_,intPath_;
