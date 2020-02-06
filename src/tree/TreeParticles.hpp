f5bb7d5fc2e3c7235deeb45bb279a3ff2beb320b
Danilo Meuser <danilo.meuser@rwth-aachen.de>
2020-02-06 11:30:13 +0100

    Changed TreeParticles to hard link

--- src/tree/TreeParticles.hpp
+++ src/tree/TreeParticles.hpp
@@ -1,1 +1,139 @@
-/home/home4/institut_1b/dmeuser/CMSSW_ttbar/CMSSW_10_2_11_patch1/src/TreeWriter/TreeWriter/plugins/TreeParticles.hpp
+#ifndef TREEPARTICLES_H
+#define TREEPARTICLES_H
+
+#include <TLorentzVector.h>
+#include <TVector3.h>
+
+enum PhotonMatchType {UNMATCHED = 0,
+                      MATCHED_FROM_GUDSCB,
+                      MATCHED_FROM_PI0,
+                      MATCHED_FROM_OTHER_SOURCES};
+
+enum PromptStatusType {
+  DIRECTPROMPT, FRAGMENTPROMPT, LEPTONPROMPT, NOPROMPT
+};
+
+namespace tree
+{
+   struct Particle
+   {
+      //~ TVector3 p;
+      TLorentzVector p;
+      bool isEB() { return fabs(p.Eta())<1.4442; }
+      bool isEE() {
+         auto aEta = fabs(p.Eta());
+         return 1.566<aEta && aEta < 2.5;
+      }
+   };
+
+   struct GenParticle: public Particle
+   {
+      Int_t pdgId=0;
+      Int_t status=0;
+      bool isPrompt;
+      bool fromHardProcess;
+      UChar_t promptStatus;
+   };
+
+   struct IntermediateGenParticle: public GenParticle
+   {
+      std::vector<GenParticle> daughters;
+   };
+
+
+   struct Jet : public Particle
+   {
+      bool isLoose;
+      bool hasElectronMatch;
+      bool hasMuonMatch;
+      float bTagCSVv2;
+      float bTagMVAv2;
+      float bTagDeepCSV;
+      float bTagSoftMuon;
+      float bTagSoftElectron;
+      float uncert;
+      float chf;
+      float nhf;
+      float cef;
+      float nef;
+      int nch;
+      int nconstituents;
+      float ptRes;
+      float phiRes;
+      float sfRes;
+      float sfResUp;
+      float sfResDn;
+      float uncorJecFactor; // uncorrected jet momentum over corrected jet momentum
+   };
+
+   struct Muon: public Particle
+   {
+      Char_t charge; // +/- 1
+      bool isTight;
+      bool isMedium;
+      bool isLoose;
+      // PF-based combined relative isolation with Δβ correction:
+      // (∑pT(ch.had from PV) + max(0, ∑ET(neut.had) + ∑ET(phot) − 0.5*∑pT(ch.had from PU)))/pT(μ)
+      float rIso;
+      float d0;
+      float dZ;
+      float PFminiIso;
+   };
+
+   struct Electron: public Particle
+   {
+      Char_t charge; // +/- 1
+      bool isLoose;
+      bool isMedium;
+      bool isTight;
+      float rIso;
+      float d0;
+      float dZ;
+      Float_t seedCrystalE;
+      float r9;
+      float SigmaIEtaIEtaFull5x5;
+      float dPhiAtVtx;
+      float dEtaAtVtx;
+      float HoverE;
+      float EoverPInv;
+      int MissHits;
+      bool ConvVeto;
+      float PFminiIso;
+      TVector3 pUncorrected;
+      float phiSC;     //Supercluster Phi
+      float etaSC;     //Supercluster Eta
+   };
+   
+   struct Photon : public Particle
+   {
+      float sigmaIetaIeta; // full 5x5
+      float hOverE;
+      bool hasPixelSeed;
+      bool passElectronVeto;
+
+      float cIso;
+      float nIso;
+      float pIso;
+
+      // IDs
+      bool  isLoose;
+      bool  isMedium;
+      bool  isTight;
+   };
+
+   struct MET : public Particle
+   {
+      Float_t  uncertainty;
+      Float_t  sig; // MET significance
+   };
+
+   inline bool PtGreater(const tree::Particle p1, const tree::Particle p2) {
+      return p1.p.Pt() > p2.p.Pt();
+   }
+   
+   inline bool PtGreaterLorentz(const TLorentzVector p1, const TLorentzVector p2) {
+      return p1.Pt() > p2.Pt();
+   }
+
+} // end namespace definition
+#endif /* TREEPARTICLES_H */
