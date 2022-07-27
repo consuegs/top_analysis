f1caff35ecff98bfb6ed0e4c4f1be17536827554
Danilo Meuser <danilo.meuser@rwth-aachen.de>
2022-07-27 12:15:05 +0200

    Change to dcap for TUnfold grid jobs and add cleaning

--- multiprocess/runGrid.sh
+++ multiprocess/runGrid.sh
@@ -6,6 +6,8 @@ sourceDir=`pwd`
 echo $(date)
 export SCRAM_ARCH=slc7_amd64_gcc820
 source /cvmfs/cms.cern.ch/cmsset_default.sh
+export HOSTNAME=$HOSTNAME
+echo $HOSTNAME
 
 #get CMSSW from dCache and setup environment
 echo $(date)
@@ -43,10 +45,10 @@ else
    mkdir ../100.0
    cd "../100.0"
    eval `scram unsetenv -sh`;
-   gfal-copy -r -t 7200 srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN="${10}"/minTrees/"$4"/"$9"/minTrees/100.0/Nominal ./Nominal
+   gfal-copy -r -t 7200 dcap://grid-dcap-extern.physik.rwth-aachen.de"${10}"/minTrees/"$4"/"$9"/minTrees/100.0/Nominal ./Nominal
    if [[ $syst != "Nominal" ]]
    then
-      gfal-copy -r -t 7200 srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN="${10}"/minTrees/"$4"/"$9"/minTrees/100.0/$syst ./$syst
+      gfal-copy -r -t 7200 dcap://grid-dcap-extern.physik.rwth-aachen.de"${10}"/minTrees/"$4"/"$9"/minTrees/100.0/$syst ./$syst
    fi
    cd ../CMSSW_10_5_0/src/
    eval `scramv1 runtime -sh`
@@ -57,8 +59,6 @@ fi
 echo $(date)
 
 export ANALYSIS_YEAR_CONFIG="$4"
-export HOSTNAME=$HOSTNAME
-echo $HOSTNAME
 
 #run framework
 if [[ $2 != "TUnfold_binning" ]]
@@ -84,6 +84,13 @@ then
    echo "Copy minTree output to dCache"
    eval `scram unsetenv -sh`; gfal-copy -r -f ../minTrees/ srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN="$7"/minTrees/"$4"/"$9"/minTrees/
    rm -r ../minTrees/
+fi
+
+#remove TUnfold_binning inputs
+if [[ $2 == "TUnfold_binning" ]]
+then
+   echo "Removing TUnfold inputs"
+   rm -r ../100.0/
 fi
 
 exit 0
